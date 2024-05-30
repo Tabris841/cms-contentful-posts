caa20a47324d88cf6d842c17a5f2fad4fbe8aee0
atodor84 <atodor@softserveinc.com>
2024-05-31 00:55:39 +0300

    working ot setting up codegen

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,9 @@
-export default function Posts() {
+import { client } from '@/lib/client'
+
+export default async function Posts() {
+  // const gqlClient = preview ? previewClient : client
+
+  // const data = await client.
+
   return <div>Posts</div>
 }
